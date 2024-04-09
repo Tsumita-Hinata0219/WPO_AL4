8102cdaee37c83a0f182a504f21505a3e9cf0af9
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-04-10 02:49:44 +0900

    PlayerReticle追加

--- Project/GameObject/Character/Player/Reticle/PlayerReticle.cpp
+++ Project/GameObject/Character/Player/Reticle/PlayerReticle.cpp
@@ -1,1 +1,17 @@
 #include "PlayerReticle.h"
+
+void PlayerReticle::Init()
+{
+}
+
+void PlayerReticle::Update()
+{
+}
+
+void PlayerReticle::Draw3D()
+{
+}
+
+void PlayerReticle::Draw2D()
+{
+}
--- Project/GameObject/Character/Player/Reticle/PlayerReticle.h
+++ Project/GameObject/Character/Player/Reticle/PlayerReticle.h
@@ -1,5 +1,49 @@
 #pragma once
-class PlayerReticle
-{
-};
 
+#include "GameObject.h"
+
+class Player;
+
+class PlayerReticle {
+
+public:
+
+	PlayerReticle() {};
+	~PlayerReticle() {};
+
+	void Init();
+	void Update();
+	void Draw3D();
+	void Draw2D();
+
+#pragma region Get
+
+	WorldTransform GetWorldTransform() { return this->wt3D_; }
+	Vector3 GetWorldPosition() { return this->wt3D_.GetWorldPos(); }
+
+#pragma endregion 
+
+#pragma region Set
+
+	void SetPlayer(Player* player) { this->player_ = player; }
+	void SetParent(WorldTransform* parent) { this->wt3D_.SetParent(parent); }
+
+#pragma endregion
+
+
+private:
+
+	Player* player_ = nullptr;
+	unique_ptr<Model> model_ = nullptr;
+	unique_ptr<Sprite> sprite_ = nullptr;
+	uint32_t reticleTexHD_;
+	WorldTransform wt3D_{};
+	WorldTransform wt2D_{};
+	const float kDistReticle_ = 60.0f;
+	Vector3 offsetVec_{};
+	Vector2 size_{};
+	Vector3 move_{};
+	Vector3 velocity_{};
+	Vector2 rStick_{};
+	Vector2 spritePos_{};
+};
